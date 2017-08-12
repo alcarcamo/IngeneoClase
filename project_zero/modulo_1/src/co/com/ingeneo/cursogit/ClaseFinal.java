0d5b8c2ac1d9dc2f25834db20eacc379f27d73e2
alejcacs <alejandro.carcamo@ingeneo.com.co>
2017-08-12 09:19:54 -0500

    Creación de clase final

---
+++ project_zero/modulo_1/src/co/com/ingeneo/cursogit/ClaseFinal.java
@@ -0,0 +1,22 @@
+package co.com.ingeneo.cursogit;
+
+public class ClaseFinal {
+	
+	public String atributo1 = "001";
+	public int a;
+	public String getAtributo1() {
+		return atributo1;
+	}
+	public void setAtributo1(String atributo1) {
+		this.atributo1 = atributo1;
+	}
+	public int getA() {
+		return a;
+	}
+	public void setA(int a) {
+		this.a = a;
+	}
+	
+	
+
+}
